099b8c3c96c6272ec151ad74e68f406e6eb8e290
FabianTrafada <fabiantrafadaperkasa@gmail.com>
2024-09-25 02:48:52 +0700

    Add Scroll header

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,11 +32,25 @@
 
 
 /*=============== HOME SWIPER ===============*/
+var homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
 
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+  });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
 
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
+}
 
+window.addEventListener('scroll', scrollHeader);
 /*=============== NEW SWIPER ===============*/
 
 
